393658352d82415dce2b786a9c36c84acda9ccf3
Flemmard <flemmard@gmail.com>
2014-11-30 13:20:01 +0100

    Add platform-specific SELinux policies
    Change-Id: I708112a4957a88263ebd48282a3994d62301618f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,10 +122,6 @@ BOARD_USES_LEGACY_MMAP := true
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/htc/evita/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-    property_contexts \
-    recovery.te
-
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 37
 
